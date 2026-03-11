5e373d3fe420e91099ab7468316812e392e06aa5
Quantum Lab <267273379+unflorts@users.noreply.github.com>
2026-03-11 15:30:48 +0530

    Add kinetic energy calculator

---
+++ physics_calculator.py
@@ -0,0 +1,8 @@
+# Physics Calculator - Quantum Lab
+
+mass = float(input("Enter mass (kg): "))
+velocity = float(input("Enter velocity (m/s): "))
+
+kinetic_energy = 0.5 * mass * velocity**2
+
+print("Kinetic Energy =", kinetic_energy, "Joules")
